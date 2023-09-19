479bf6a7722cb34bb37f1e48a4e269213bbc2409
John Stuby <johnstuby7@gmail.com>
2023-09-19 12:06:22 -0600

    Created a figma logo for the movie picker app and added it to the header

--- movie-picker-project/src/components/Header.js
+++ movie-picker-project/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Header.css";
+import LogoMoviePicker from "../assets/MoviePickerLogoGood.svg";
 
 function Header() {
   return (
     <div className="Header">
       <div>
+        <img src={LogoMoviePicker} className="popcorn-logo" alt="logo" />
         <h1>Movie Picker!</h1>
         <p>Your one stop location to pick a movie</p>
       </div>
