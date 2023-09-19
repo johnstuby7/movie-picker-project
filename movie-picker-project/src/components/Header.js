9995b28c684fc8601cd9f8d8940172bfd5726f6d
John Stuby <johnstuby7@gmail.com>
2023-09-19 11:10:42 -0600

    Initial commit for the header component, added in basic component that has a Title, description, will be adding in a logo of some sort

---
+++ movie-picker-project/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <div>
+        <h1>Movie Picker!</h1>
+        <p>Your one stop location to pick a movie</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
