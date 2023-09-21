2ceab54cdcc9056318247ad4828aa041ab60f611
John Stuby <johnstuby7@gmail.com>
2023-09-21 14:14:21 -0600

    Added in valueChangeHandler, inputBlurHandler and reset to handle when a value is entered and to handle the reset action for fields

--- movie-picker-project/src/hooks/user-input.js
+++ movie-picker-project/src/hooks/user-input.js
@@ -6,6 +6,19 @@ const userInput = (validateValue) => {
 
   const valueIsValid = validateValue(enteredValue);
   const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
 };
 
 export default userInput;
